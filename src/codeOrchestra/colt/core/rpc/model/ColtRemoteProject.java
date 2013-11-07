8a926ce80dfab1c5050d6dce55ef431afc6f1cca
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-11-07 17:21:19 +0400

    [CJ-825]

---
+++ src/codeOrchestra/colt/core/rpc/model/ColtRemoteProject.java
@@ -0,0 +1,12 @@
+package codeOrchestra.colt.core.rpc.model;
+
+/**
+ * @author Alexander Eliseyev
+ */
+public abstract class ColtRemoteProject {
+
+    public abstract String getPath();
+
+    public abstract String getName();
+
+}
